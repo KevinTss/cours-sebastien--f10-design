ec61f4b5a3bbeeeb3943220cb58ca2b086d1b375
KevinTss <kevin.tassi@gmail.com>
2021-01-30 17:07:41 +0100

    Add first exercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return <div className='App'>Welcome</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("--constructor--", props);
+  }
+
+  // utiliser componentDidMount()
+  // pour recuperer les données de JSON place holder: https://jsonplaceholder.typicode.com/
+  // Et de les ajouter au state
+  // et de les afficher dans le render
+
+  render() {
+    console.log("--render--");
+    return <div className="App">Welcome</div>;
+  }
 }
 
 export default App;
