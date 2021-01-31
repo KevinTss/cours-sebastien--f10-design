b1c99c0a61a2a5f25388380c954d93f240324e57
KevinTss <kevin.tassi@gmail.com>
2021-01-31 16:25:27 +0100

    fix: Remove useless code
    - Remove useless comments
    - Remove useless file
    
    See [pull request](https://github.com/KevinTss/cours-sebastien--f10-design/pull/1)

--- src/App.js
+++ src/App.js
@@ -3,37 +3,27 @@ import "./App.css";
 
 class App extends React.Component {
   state = {
-    albums: []
-  }
+    albums: [],
+  };
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/albums')
+    fetch("https://jsonplaceholder.typicode.com/albums")
       .then((response) => response.json())
-      .then(albumsList => {
-        console.log(albumsList)
+      .then((albumsList) => {
+        console.log(albumsList);
         this.setState({ albums: albumsList });
       });
   }
 
-  componentWillUnmount() {
-    // make fetch request
-  }
-
   render() {
     return (
-      // <ul>
-      //   {this.state.albums.map((album) => (
-      //     <li key={album.id}>{album.title}</li>
-      //   ))}
-      // </ul>
       <React.Fragment>
         {this.state.albums.map((album) => (
           <div key={album.id}>{album.title}</div>
         ))}
       </React.Fragment>
-    )
+    );
   }
 }
-
 
 export default App;
